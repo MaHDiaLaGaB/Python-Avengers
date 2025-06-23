623cd22a64e561bc17d1ca1d4353120f37780e2d
MaHDi <64260599+MaHDiaLaGaB@users.noreply.github.com>
2025-06-23 17:04:40 +0000

    first init

---
+++ app/input.py
@@ -0,0 +1,4 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+
+print(f"Hello, {name}!, Next year your age will be {int(age) + 1}")
---
+++ app/operatprs.py
@@ -0,0 +1,21 @@
+a, b = 9, 7
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+print(a ** b)
+
+print(a > b)
+print(a < b)
+print(a == b)
+print(a != b)
+print(a >= b)
+print(a >= b and b == a)
+print(a <= b or a != b)
+
+counter = 0
+counter = counter + 1
+counter -= 1
+
