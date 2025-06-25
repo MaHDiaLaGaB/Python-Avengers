ca3c9157ef0ffe85b83ac2dbdda976dc3bf6dfcb
MaHDi <64260599+MaHDiaLaGaB@users.noreply.github.com>
2025-06-25 14:29:28 +0000

    add list ex

---
+++ app/flow_control.py
@@ -0,0 +1,17 @@
+# degree = int(input("enter the degree: "))
+
+# if degree > 99:
+#     print("A")
+
+# if degree < 100 and degree >= 90:
+#     print("B")
+
+# if degree <= 90 and degree >= 80:
+#     print("C")
+
+
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+my_list.insert(6, 60)
+
+print(my_list)
---
+++ homeworks/temperature.py
@@ -0,0 +1,3 @@
+c = float(input("temp in c: "))
+f = c * (9/5) + 32
+print(f"the temp in F is {f}")
