1c93081304b8a5b2ee18d9051fbbbf3712cf45c5
MaHDi <64260599+MaHDiaLaGaB@users.noreply.github.com>
2025-07-03 16:11:46 +0000

    modify

--- app/classes.py
+++ app/classes.py
@@ -13,18 +13,36 @@ class Calculator:
         Calculator.ins_count += 1
 
     def calculator(self, op: Operations, a, b):
-        match op:
-            case Operations.ADD: res = a + b
-            case Operations.SUB: res = a - b
+        if self.is_number(a) and self.is_number(b):
+            match op:
+                case Operations.ADD: res = a + b
+                case Operations.SUB: res = a - b
 
-        self.last_result = res
-        return res
+            self.last_result = res
+            return res
+    
+    @classmethod
+    def reset_counter(cls):
+        cls.ins_count = 0
+
+    @staticmethod
+    def is_number(value: str) -> bool:
+        try:
+            float(value)
+            return True
+        except ValueError:
+            return False
 
     
 cal_1 = Calculator("A")
 result = cal_1.calculator(Operations.ADD, 10, 20)
 cal_2 = Calculator("B")
+cal_3 = Calculator("C")
 result_2 = cal_2.calculator(Operations.SUB, 300, 234)
+result_3 = cal_3.calculator(Operations.ADD, 234, 345)
+print(result_3)
+Calculator.reset_counter()
+print(cal_1.ins_count)
 
-print(result)
-print(result_2)
+# print(result)
+# print(result_2)
